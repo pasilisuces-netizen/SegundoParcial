2845a9d9e2535b032088ce5e910200def624fe26
armandopasilis <34778740+armandopasilis@users.noreply.github.com>
2026-06-24 21:43:30 -0300

    correcciones allowed_host

--- Neural_Tech/Neural_Tech/settings.py
+++ Neural_Tech/Neural_Tech/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)0%%7w!zcn42hvn)4&sx0$-zjh4w%r(4qdbkvig36jh_k)hy(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://segundoparcial-5ptl.onrender.com']
+ALLOWED_HOSTS = ['segundoparcial-5ptl.onrender.com']
 
 
 # Application definition
